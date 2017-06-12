8b47db90b4e4f05538271143083aa1efccb45d3d
Abtin Ravanipour <Abtinr1185@gmail.com>
2017-06-11 22:15:43 -0700

    initial

---
+++ models/NYTimes.js
@@ -0,0 +1,26 @@
+// First, we hook mongoose into the model with a require
+const mongoose = require("mongoose");
+
+// Then, we save the mongoose.Schema class as simply "Schema"
+const Schema = mongoose.Schema;
+
+// With our new Schema class, we instantiate an ExampleSchema object
+// This is where we decide how our data must look before we accept it in the server, and how to format it in mongoDB
+const NYTimesSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+  }
+  link: String,
+  comments: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+// This creates our model from the above schema, using mongoose's model method
+const NYTimes = mongoose.model("NYTimes", NYTimesSchema);
+
+// Finally, we export the module, allowing server.js to hook into it with a require statement
+module.exports = NYTimes;
